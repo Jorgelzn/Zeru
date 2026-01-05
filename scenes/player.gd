2e3dc81c676b1b93600796ecc3c2f767c5f6711e
Jorgelzn <jorgelizgo@gmail.com>
2026-01-05 12:16:32 +0100

    attack animation added

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,18 +4,24 @@ const SPEED = 100.0
 
 var input_vector: = Vector2.ZERO
 @onready var animation_tree: AnimationTree = $AnimationTree
-
+@onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 func _physics_process(delta: float) -> void:
+	var state = playback.get_current_node()
+	if state == "MoveState":
+		input_vector = Input.get_vector("move_left","move_right","move_up","move_down")
 	
-	input_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	velocity = input_vector * SPEED
-	
-	if input_vector != Vector2.ZERO:
-		var direction_vector = Vector2(input_vector.x, -input_vector.y)
-		update_blend_position(direction_vector)
+		if input_vector != Vector2.ZERO:
+			var direction_vector = Vector2(input_vector.x, -input_vector.y)
+			update_blend_position(direction_vector)
+			
+		if Input.is_action_just_pressed("attack"):
+			playback.travel("AttackState")
+		velocity = input_vector * SPEED
 		move_and_slide()
+	elif state == "AttackState":
+		pass
 
 func update_blend_position(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position",direction_vector)
 	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position",direction_vector)
-	
+	animation_tree.set("parameters/StateMachine/AttackState/blend_position",direction_vector)
